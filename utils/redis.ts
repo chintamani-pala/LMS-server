f73b9fc504a0cea9c4a46425c6555a0e7ed7ab8d
CHINTAMANI PALA <chintamanipala67@gmail.com>
2024-05-05 11:47:15 +0530

    Update redis.ts

--- utils/redis.ts
+++ utils/redis.ts
@@ -1,4 +1,4 @@
-/*import { Redis } from "ioredis";
+import { Redis } from "ioredis";
 require("dotenv").config();
 const redisClient = () => {
   if (process.env.REDIS_URL) {
@@ -8,28 +8,3 @@ const redisClient = () => {
   throw new Error(`Redis connection failed`);
 };
 export const redis = new Redis(redisClient());
-*/
-
-
-import Redis from "ioredis"; // Change the import statement
-
-import dotenv from "dotenv";
-dotenv.config();
-
-const redisClient = (): string => {
-  if (process.env.REDIS_URL) {
-    console.log(`Redis Connected`);
-    return process.env.REDIS_URL;
-  }
-  throw new Error(`REDIS_URL environment variable is not defined`);
-};
-
-const redisUrl = new URL(process.env.REDIS_URL || "");
-const redisOptions:any= {
-  connectTimeout: 10000, // Set the connection timeout to 10 seconds
-  host: redisUrl.hostname,
-  port: Number(redisUrl.port), // Convert port to number
-  password: redisUrl.password
-};
-
-export const redis = new Redis(redisOptions);
